7e3f1b2e18200397fafe2e5145ea770ab6d9758e
Liluyoud Cury de Lacerda <liluyoud@outlook.com>
2021-11-09 21:39:20 -0400

    City Controller Complete

--- Escola/School.Api/Controllers/CityController.cs
+++ Escola/School.Api/Controllers/CityController.cs
@@ -22,5 +22,48 @@ namespace School.Api.Controllers
             return Ok(cities);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<City>> GetByIdAsync(long id)
+        {
+            var city = await cityRepository.GetByIdAsync(id);
+            if (city == null) return NotFound();
+            return Ok(city);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<City>> AddAsync(City city)
+        {
+            if (ModelState.IsValid)
+            {
+                await cityRepository.AddAsync(city);
+                await cityRepository.SaveAsync();
+                return CreatedAtAction(nameof(GetByIdAsync), new { Id = city.Id}, city);
+            }
+            return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateAsync(long id, City city)
+        {
+            if (ModelState.IsValid && id == city.Id)
+            {
+                if (!(await cityRepository.Exists(id))) 
+                    return NotFound();
+                cityRepository.Update(city);
+                await cityRepository.SaveAsync();
+                return NoContent();
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAsync(long id)
+        {
+            if (!(await cityRepository.Exists(id)))
+                return NotFound();
+            await cityRepository.DeleteAsync(id);
+            await cityRepository.SaveAsync();
+            return NoContent();
+        }
     }
 }
